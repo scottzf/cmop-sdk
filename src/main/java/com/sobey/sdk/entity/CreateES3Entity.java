3e37caa5ab95ed13747fd8571a6f460de606ded8
刘凯 <kai8406@gmail.com>
2015-01-26 15:08:26 +0800

    fix bug
    createES3构造函数中不应该有ecs相关信息了

--- src/main/java/com/sobey/sdk/entity/CreateES3Entity.java
+++ src/main/java/com/sobey/sdk/entity/CreateES3Entity.java
@@ -6,18 +6,16 @@ import com.sobey.sdk.constant.IDCEnum;
 public class CreateES3Entity {
 
 	private String accessKey;
-	private String ecsCode;
 	private String es3Name;
 	private Integer es3Size;
 	private ES3TypeEnum es3TypeEnum;
 	private IDCEnum idcEnum;
 	private String remark;
 
-	public CreateES3Entity(String accessKey, String ecsCode, String es3Name, Integer es3Size, ES3TypeEnum es3TypeEnum,
-			IDCEnum idcEnum, String remark) {
+	public CreateES3Entity(String accessKey, String es3Name, Integer es3Size, ES3TypeEnum es3TypeEnum, IDCEnum idcEnum,
+			String remark) {
 		super();
 		this.accessKey = accessKey;
-		this.ecsCode = ecsCode;
 		this.es3Name = es3Name;
 		this.es3Size = es3Size;
 		this.es3TypeEnum = es3TypeEnum;
@@ -27,10 +25,6 @@ public class CreateES3Entity {
 
 	public String getAccessKey() {
 		return accessKey;
-	}
-
-	public String getEcsCode() {
-		return ecsCode;
 	}
 
 	public String getEs3Name() {
@@ -55,10 +49,6 @@ public class CreateES3Entity {
 
 	public void setAccessKey(String accessKey) {
 		this.accessKey = accessKey;
-	}
-
-	public void setEcsCode(String ecsCode) {
-		this.ecsCode = ecsCode;
 	}
 
 	public void setEs3Name(String es3Name) {
